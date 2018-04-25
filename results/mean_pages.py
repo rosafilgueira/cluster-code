6c0e6fc101ed8011fb3c6083c708d8bf80a426f9
Rosa Filgueira <rosafilgueira@MBP-RF.local>
2018-04-25 22:48:28 +0100

    mean

--- results/mean_pages.py
+++ results/mean_pages.py
@@ -8,7 +8,10 @@ import yaml
 streams = get_streams(downsample = 4096)
 issues = streams.map(Archive)
 books = issues.flatMap(lambda x: list(x))
+print("books count: %s" % books.count())
 word_counts = books.map(lambda x: len(list(x.words())))
+for wc in word_counts.collect():
+	print("Word_count is %s" % wc)
 
 result = [books.count(), word_counts.reduce(lambda x,y: x+y)]
 
