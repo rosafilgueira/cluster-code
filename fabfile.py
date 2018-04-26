e495d33ef746b883b3cb1952ce0fd9fe0df5a315
Rosa Filgueira <rosafilgueira@c041177.wlan.net.ed.ac.uk>
2018-04-26 13:43:10 +0100

    fab

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,6 @@
 from deploy.local import *
 
 env.user='rosa'
-
-env.results_dir="/Users/rosafilgueira/EPCC/ATI-SE/Humanities/Rosa_Versions/cluster-code/output"
-
-env.model="bluclobber"
-env.corpus='/Users/rosafilgueira/EPCC/ATI-SE/Humanities/Rosa_Versions/cluster-code/bluclobber/test/fixtures/corpus24/'
-
-env.deploy_to="/Users/rosafilgueira/EPCC/ATI-SE/Humanities/Rosa_Versions/cluster-code/results"
+env.model='bluclobber'
+env.corpus='bluclobber/test/fixtures/corpus24/'
+env.deploy_to='results'
