fac9a5f65c57ffc06b31374d9a0de138f9f97429
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-27 13:18:59 +0100

    stand

--- deploy/standalone.py
+++ deploy/standalone.py
@@ -25,7 +25,7 @@ def install(query):
     with lcd(env.standalone_deploy_dir):  # pylint: disable=not-context-manager
         local('cp -r ../bluclobber/ .')
         local('cp ../' + query + ' ./bluclobber/harness/query_sub.py')
-        local('cp ../deploy/urika.sh ./harness/.')
+        local('cp ../deploy/urika.sh ./bluclobber/harness/.')
         local('find . -iname "*.pyc" -delete')
         local('find . -iname "__pycache__" -delete')
 
