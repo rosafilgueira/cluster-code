db632c3c4f22e3b593e7ccff6114ffff07d373e1
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-27 12:21:00 +0100

    fab

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,15 @@
-from deploy import legion
+'''
+Fab file for deployment.
+'''
+
+from deploy import standalone
+
+# deploy.remote configuration.
+env.user='rfilguei'
+env.results_dir="/home/"+env.user+"/output"
+env.model="bluclobber"
+env.corpus='/rdZone/live/rd003v/CompressedALTO-fromJamesH/CompressedALTO'
+env.deploy_to="/home/"+env.user+"/BluclobberSpark"
+
+# deploy.standalone configuration.
+env.standalone_deploy_dir = 'standalone'
