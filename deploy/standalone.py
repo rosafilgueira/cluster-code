c9d4cb86bdab4f30ca679efb75c372f883a2b519
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-27 13:01:48 +0100

    standalone

---
+++ deploy/standalone.py
@@ -0,0 +1,48 @@
+'''
+Set up to run locally. This is designed to be used for testing.
+'''
+
+import os
+import pandas as pd
+from fabric.api import task, env, execute, lcd, local
+
+
+@task
+def setup(query):
+    '''
+    Prepare instance for running. Generates necessary files.
+    '''
+    execute(install, query=query)
+
+
+@task
+def install(query):
+    '''
+    Generate necessary files.
+    '''
+    local('rm -rf ' + env.standalone_deploy_dir)
+    local('mkdir -p ' + env.standalone_deploy_dir)
+    with lcd(env.standalone_deploy_dir):  # pylint: disable=not-context-manager
+        local('cp -r ../bluclobber/harness/* .')
+        local('cp ../' + query + ' ./query_sub.py')
+        local('cp ../deploy/urika.sh .')
+        local('find . -iname "*.pyc" -delete')
+        local('find . -iname "__pycache__" -delete')
+
+
+@task
+def test():
+    '''
+    Run the query.
+    '''
+    with lcd(env.standalone_deploy_dir):  # pylint: disable=not-context-manager
+        local('./urika.sh query_sub.py')
+
+
+@task
+def pytest():
+    '''
+    Run pytest tests.
+    '''
+    with lcd(env.standalone_deploy_dir):  # pylint: disable=not-context-manager
+        local('py.test')
