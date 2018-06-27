bf3fb6d121e1f6bafef759021c808b2a48f0218d
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-27 12:55:07 +0100

    new fab

--- fabfile.py
+++ fabfile.py
@@ -2,14 +2,9 @@
 Fab file for deployment.
 '''
 
+from fabric.api import env
 from deploy import standalone
-
-# deploy.remote configuration.
-env.user='rfilguei'
-env.results_dir="/home/"+env.user+"/output"
-env.model="bluclobber"
-env.corpus='/rdZone/live/rd003v/CompressedALTO-fromJamesH/CompressedALTO'
-env.deploy_to="/home/"+env.user+"/BluclobberSpark"
+from deploy import legion
 
 # deploy.standalone configuration.
 env.standalone_deploy_dir = 'standalone'
