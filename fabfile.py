d0b3f1f56d571660b5654ca805b53913f533bfd8
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-27 13:25:03 +0100

    fab

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,6 @@ Fab file for deployment.
 
 from fabric.api import env
 from deploy import standalone
-from deploy import legion
 
 # deploy.standalone configuration.
 env.standalone_deploy_dir = 'standalone'
