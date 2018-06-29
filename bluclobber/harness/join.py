8b704dba9084d468e44810bd3717f7d8ba859b2a
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-06-29 09:11:53 +0100

    updated join

--- bluclobber/harness/join.py
+++ bluclobber/harness/join.py
@@ -2,9 +2,10 @@ import sys
 import os
 
 file_year=["1510_1699", "1700_1799", "1800_1809", "1810_1819", "1820_1829","1830_1839", "1840_1849", "1850_1859", "1860_1869", "1870_1879", "1880_1889", "1890_1899"]
-path = "./output/"
 import shutil
-np= sys.argv[1]
+path= sys.argv[1]
+np= sys.argv[2]
+python join.py $output $NP
 for i in file_year:
 	filename=path+"out_"+i
 	outfilename=path+"final_"+i+".yml"
