fd45336c412323d6e92412b54245b678b51fa7a9
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-07-04 22:33:08 +0100

    urika mode

---
+++ deploy/urika_login.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x 
+
+query=$1
+NP='16'
+name=$2
+output='./output_'$name'/'
+final='./data_'$name'/'
+remote_directory='/mnt/lustre/rfilguei/dch/BritishLibraryBooks'
+
+rm -rf $output
+rm -rf $final
+
+mkdir $output
+mkdir $final
+
+for i in $remote_directory/1*; do
+  if [ -d "$i" ]; then
+	path=$i
+	outputpath=$output
+	year=${i##*/}
+	outputpath+="out_"$year
+        echo $NP $query $path $outputpath
+        ./run_query.sh $NP $query $path $outputpath
+    
+  fi
+done
+
+
+python join_$name'.py' $output $NP
+python result_$name'.py' $output $final
