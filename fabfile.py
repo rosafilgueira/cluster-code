36e5bf711d737e456c642296e90705d7aff27f9b
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-07-05 09:40:10 +0100

    urika

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,8 @@ Fab file for deployment.
 
 from fabric.api import env
 from deploy import standalone
+from deploy import urika
 
 # deploy.standalone configuration.
 env.standalone_deploy_dir = 'standalone'
+env.production_deploy_dir = 'production'
