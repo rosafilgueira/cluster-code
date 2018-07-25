503af94ae77e00ee615238060c27395af6f16376
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-07-25 17:40:10 +0100

    Changed compare.py output

--- bluclobber/tools/compare.py
+++ bluclobber/tools/compare.py
@@ -16,9 +16,8 @@ def compare_keys(file1, data1, file2, data2):
     keys2 = data2.keys()
     diff = [key for key in keys1 if key not in keys2]
     if len(diff) != 0:
-        print("Key-values in %s only:" % file1)
         for key in diff:
-            print("%10s: %10s" % (key, data1[key]))
+            print("%s: %s (%s)" % (key, data1[key], file1))
 
 
 def compare_values(file1, data1, file2, data2):
@@ -28,7 +27,7 @@ def compare_values(file1, data1, file2, data2):
         if (key not in keys2):
             continue
         if data1[key] != data2[key]:
-            print("Different value for key: %10s: %10s %10s" % 
+            print("%s: %s =/= %s" % 
                 (key, data1[key], data2[key]))
 
 
@@ -39,6 +38,8 @@ with open(file1, "r") as f:
     data1 = yaml.load(f)
 with open(file2, "r") as f:
     data2 = yaml.load(f)
+
+print("%s %s" % (file1, file2))
 
 compare_keys(file1, data1, file2, data2)
 compare_keys(file2, data2, file1, data1)
@@ -52,9 +53,7 @@ for key in keys1:
     value2 = data2[key]
     if type(value1) is list and type(value2) is list:
         if value1 != value2:
-            print("Different value for key: %10s" % key)
-            print("%s" % value1)
-            print("%s" % value2)
+            print("%s: %s =/= %s" % (key, value1, value2))
     else:
         keys1 = value1.keys()
         keys2 = value2.keys()
